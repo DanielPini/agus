b807b0f7048b27a7b92356e0696e40996b9d8c7c
DanielPini <daniel@danielpini.com>
2026-07-13 07:21:25 +0200

    feat(iframe): switch implementation to work with vimeo. Add bundler for kirby

--- captions-types.js
+++
@@ -1,1 +0,0 @@
-export const languages = ["english", "french", "german", "indonesian"];
--- script.js
+++
@@ -1,8 +0,0 @@
-import { initVideoPlayer } from "./video.js";
-import { initCaptionsPlayer } from "./captions-player.js";
-import { initSettingsPicker } from "./settings-picker.js";
-import { initSettingsMenus } from "./settings-menu.js";
-initVideoPlayer();
-initCaptionsPlayer();
-initSettingsPicker();
-initSettingsMenus();
